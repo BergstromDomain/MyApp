6017457cb4248cec2afe3ec55a10aab152f9bb0d
Niklas Bergstrom <me.niklas@gmail.com>
2021-07-03 06:59:15 +0000

    Customized the routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,17 +1,19 @@
 Rails.application.routes.draw do
-  resources :portfolios
-  resources :blogs
   root to: 'pages#home'
 
-  get 'home',           to: 'pages#home'
-  get 'about',          to: 'pages#about'
-  get 'contact',        to: 'pages#contact'
+  get 'about-me',       to: 'pages#about'
+  get 'contact',     to: 'pages#contact'
   get 'event-tracker',  to: 'pages#event_tracker'
   
+  resources :blogs
+
   resources :people do
     resources :events
   end
   resources :events, :only => [:index]
 
+  resources :portfolios, except: [:show]
+  get 'portfolio/:id', to: 'portfolios#show', as: 'portfolio_show'
+  
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
